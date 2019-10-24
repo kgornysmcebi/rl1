3f0ce042d86b29c5218b8a8afd19745d7e0e6c7c
Sektus <timur.porokhnia@smcebi.edu.pl>
2019-10-24 16:20:49 +0200

    New line after hello world

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() 
 {
-    cout << "Hello, World!";
+    cout << "Hello, World!\n";
     cout << "Bye!!\r\n";
     for (int i=0; i<10; i++)
 	cout<<"bye in "<<10-i<<"\n";
